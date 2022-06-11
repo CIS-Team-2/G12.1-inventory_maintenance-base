8155a0c6a12cc58d7aa71ddf20f4ccdbe3fd9c48
tepper-d <104649969+tepper-d@users.noreply.github.com>
2022-06-11 12:38:28 -0700

    comment added to all .cs files

--- InventoryMaintenance/InvItemList.cs
+++ InventoryMaintenance/InvItemList.cs
@@ -2,6 +2,14 @@
 using System.Collections.Generic;
 using System.Text;
 
+/***************************************************************************************************
+* CIS 123: Introduction to OOP     |    Exercise 14-1 Use inheritance                              *
+* Murach's C#, 7th Edition         |    Add 2 classes to the Inventory Maintenance application     *
+* Chapter 14 Extra Exercises       |    that inherit the InvItem class. Add code to the forms to   *
+* Team 2 Assignment, 11JUN2022     |    provide for these new classes:                             *
+* Patrick McKee & Dominique Tepper |           (1) Plant, (2) Supply                               *
+***************************************************************************************************/
+
 namespace InventoryMaintenance
 {
     public class InvItemList
--- InventoryMaintenance/Program.cs
+++ InventoryMaintenance/Program.cs
@@ -4,6 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/***************************************************************************************************
+* CIS 123: Introduction to OOP     |    Exercise 14-1 Use inheritance                              *
+* Murach's C#, 7th Edition         |    Add 2 classes to the Inventory Maintenance application     *
+* Chapter 14 Extra Exercises       |    that inherit the InvItem class. Add code to the forms to   *
+* Team 2 Assignment, 11JUN2022     |    provide for these new classes:                             *
+* Patrick McKee & Dominique Tepper |           (1) Plant, (2) Supply                               *
+***************************************************************************************************/
+
 namespace InventoryMaintenance
 {
     static class Program
