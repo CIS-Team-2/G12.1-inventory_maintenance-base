285749f616330878bd7449511f7c19fe132999b1
tepper-d <104649969+tepper-d@users.noreply.github.com>
2022-06-11 12:35:34 -0700

    Add project files.

---
+++ InventoryMaintenance/frmInvMaint.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/***************************************************************************************************
+* CIS 123: Introduction to OOP     |    Exercise 14-1 Use inheritance                              *
+* Murach's C#, 7th Edition         |    Add 2 classes to the Inventory Maintenance application     *
+* Chapter 14 Extra Exercises       |    that inherit the InvItem class. Add code to the forms to   *
+* Team 2 Assignment, 11JUN2022     |    provide for these new classes:                             *
+* Patrick McKee & Dominique Tepper |           (1) Plant, (2) Supply                               *
+***************************************************************************************************/
+
+namespace InventoryMaintenance
+{
+    public partial class frmInvMaint : Form
+    {
+        public frmInvMaint()
+        {
+            InitializeComponent();
+        }
+
+        private InvItemList invItems = new InvItemList();
+
+        private void frmInvMaint_Load(object sender, EventArgs e)
+        {
+            invItems.Changed += new InvItemList.ChangeHandler(HandleChange);
+            invItems.Fill();
+            FillItemListBox();
+        }
+
+        private void FillItemListBox()
+        {
+            InvItem item;
+            lstItems.Items.Clear();
+            for (int i = 0; i < invItems.Count; i++)
+            {
+                item = invItems[i];
+                lstItems.Items.Add(item.GetDisplayText());
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            frmNewItem newItemForm = new frmNewItem();
+            InvItem invItem = newItemForm.GetNewItem();
+            if (invItem != null)
+            {
+                invItems += invItem;
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int i = lstItems.SelectedIndex;
+            if (i != -1)
+            {
+                InvItem invItem = invItems[i];
+                string message = "Are you sure you want to delete "
+                    + invItem.Description + "?";
+                DialogResult button =
+                    MessageBox.Show(message, "Confirm Delete",
+                    MessageBoxButtons.YesNo);
+                if (button == DialogResult.Yes)
+                {
+                    invItems -= invItem;
+                }
+            }
+        }
+
+        private void HandleChange(InvItemList invItems)
+        {
+            invItems.Save();
+            FillItemListBox();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
